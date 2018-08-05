ef96d10470d724cc4acc27fdb1fcb4c648e87226
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-08-05 20:54:13 +0300

    add mainjs + menu visibility

---
+++ build/scripts/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    var $butt = $('#hcb'),
+        $menu = $('#menu'),
+        $ws5  = $('.header__whitespace5'),
+        $ws6  = $('.header__whitespace6'),
+        $hg   = $('.header-catalog'),
+        $hs   = $('.header-conditions'),
+        $hmc  = $('#hmc')
+
+    $butt.on('click', function(){
+        $menu.toggleClass('menu-visible');
+        $ws5.toggleClass('header-bottomline-clicked');
+        $ws6.toggleClass('header-bottomline-clicked');
+        $hg.toggleClass('header-bottomline-clicked');
+        $hs.toggleClass('header-bottomline-clicked');
+        $butt.toggleClass('header-catalog-button-clicked');
+        console.log('clicked all');
+    });
+
+    $hmc.on('click', function() {
+        $menu.removeClass('menu-visible');
+        $ws5.removeClass('header-bottomline-clicked');
+        $ws6.removeClass('header-bottomline-clicked');
+        $hg.removeClass('header-bottomline-clicked');
+        $hs.removeClass('header-bottomline-clicked');
+        $butt.removeClass('header-catalog-button-clicked');
+    });
+});
